942a9960677f5fec61bf440081cf17f83542dcdf
h0lger <audidave@gmail.com>
2013-06-06 20:38:32 +0200

    Created class library project

---
+++ KataPotterLib/KataPotterLib/Media.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataPotterLib
+{
+    /// <summary>
+    /// Base class
+    /// </summary>
+    public class Media
+    {
+        public double Price { get; set; }
+        public string Title { get; set; }
+    }
+}
