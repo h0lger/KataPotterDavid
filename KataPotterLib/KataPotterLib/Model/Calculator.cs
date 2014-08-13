d7eebc3180ff9c1a558e4e8f67e05251ea7660f9
David <audidave@gmail.com>
2014-08-13 17:29:27 +0200

    Started on new discount algorithm

--- KataPotterLib/KataPotterLib/Model/Calculator.cs
+++ KataPotterLib/KataPotterLib/Model/Calculator.cs
@@ -7,86 +7,95 @@ using System.Threading.Tasks;
 
 namespace KataPotterLib.Model
 {
-    public class Calculator
+  public class Calculator
+  {
+    #region Methods
+    /// <summary>
+    /// Calculates total price incl discount
+    /// </summary>
+    /// <param name="sCart"></param>
+    /// <returns></returns>
+    public static double CalcTotalPrice(ShoppingCart sCart)
     {
-        #region Methods
-        /// <summary>
-        /// Calculates total price incl discount
-        /// </summary>
-        /// <param name="sCart"></param>
-        /// <returns></returns>
-        public static double CalcTotalPrice(ShoppingCart sCart)
-        {
-            double totPrice = 0;
+      double totPrice = 0;
 
-            sCart.Items.ToList().ForEach(x => totPrice += x.Price);
+      sCart.Items.ToList().ForEach(x => totPrice += x.Price);
 
-            return (totPrice - CalcDiscount(sCart, totPrice));
-        }
-
-        public static double CalcDiscount(ShoppingCart sCart, double totPrice)
-        {
-            double discount = 0;
-
-            //Calc Harry Potter series discount           
-
-
-          var allCombos = CalcCombinations(sCart.Items.ToList());
-
-          allCombos.ForEach(x =>
-            {
-              
-            });
-  
-          return discount;
-        }
-
-        private double CalcSetDiscount(int nDifferentTitles, int nBooks, double price)
-        {
-            switch (nDifferentTitles)
-            {
-                case 5:
-                    return nBooks * price * 0.75;
-                case 4:
-                    return nBooks * price * 0.80;
-                case 3:
-                    return nBooks * price * 0.90;
-                case 2:
-                    return nBooks * price * 0.95;
-                case 1:
-                    return nBooks * price * 1;
-
-                default:
-                    throw new InvalidOperationException();
-            }
-        }
-
-
-        private static IEnumerable<int> ConstructSetFromBits(int i)
-        {
-            for (int n = 0; i != 0; i /= 2, n++)
-            {
-                if ((i & 1) != 0)
-                    yield return n;
-            }
-        }
-
-
-        private static IEnumerable<List<Media>> ProduceEnumeration(List<Media> allValues)
-        {
-            for (int i = 0; i < (1 << allValues.Count); i++)
-            {
-                yield return
-                    ConstructSetFromBits(i).Select(n => allValues[n]).ToList();
-            }
-        }
-
-        public static List<List<Media>> CalcCombinations(List<Media> allValues)
-        {
-            return ProduceEnumeration(allValues).ToList();
-        }
-              
+      return (totPrice - CalcDiscount(sCart, totPrice));
     }
+
+    public static double CalcDiscount(ShoppingCart sCart, double totPrice)
+    {
+      double discount = 0;
+
+      //Calc Harry Potter series discount
+
+      var uniqueTitles = sCart.Items.Select(x => x.Title).Distinct();
+      foreach (string uTitle in uniqueTitles)
+      {
+        double price = sCart.Items.First(x => x.Title == uTitle).Price;
+        discount += CalcSetDiscount(sCart.Items.Count(x => x.Title == uTitle), price);
+      }
+
+      //var allCombos = CalcCombinations(sCart.Items.ToList());
+
+      //allCombos.ForEach(x =>
+      //  {
+
+      //  });
+
+      return discount;
+    }
+
+    private static double CalcSetDiscount(int nBooks, double price)
+    {
+      switch (nBooks)
+      {
+        //case 5:
+        //  return nBooks * price * 0.75; //25%
+        case 4:
+          return nBooks * price * 0.20; //20%
+        case 3:
+          return nBooks * price * 0.10; //10%
+        case 2:
+          return nBooks * price * 0.05; //5%
+        case 1:
+          return 0;
+        case 0:
+          return 0;
+
+          // >=5 books
+        default:
+          return nBooks * price * 0.75; //25%
+      }
+    }
+
+
+    private static IEnumerable<int> ConstructSetFromBits(int i)
+    {
+      for (int n = 0; i != 0; i /= 2, n++)
+      {
+        if ((i & 1) != 0)
+          yield return n;
+      }
+    }
+
+
+    private static IEnumerable<List<Media>> ProduceEnumeration(List<Media> allValues)
+    {
+      for (int i = 0; i < (1 << allValues.Count); i++)
+      {
+        yield return
+            ConstructSetFromBits(i).Select(n => allValues[n]).ToList();
+      }
+    }
+
+    public static List<List<Media>> CalcCombinations(List<Media> allValues)
+    {
+      return ProduceEnumeration(allValues).ToList();
+    }
+
+  }
 }
-      
-#endregion
+
+    #endregion
--- KataPotterLib/UnitTest/UnitTests.cs
+++ KataPotterLib/UnitTest/UnitTests.cs
@@ -6,79 +6,87 @@ using KataPotterLib.Utils;
 
 namespace UnitTest
 {
-    [TestClass]
-    public class UnitTests
+  [TestClass]
+  public class UnitTests
+  {
+    [TestMethod]
+    public void AddRemoveBooks()
     {
-        [TestMethod]
-        public void AddRemoveBooks()
-        {
-            ShoppingCart SCart = new ShoppingCart();
-            //Creation of 100 books
-            for (int i = 0; i < 100; i++)
-            {
-                Book tmpBook = new Book(
-                    (100 * i), "TestBook", Guid.NewGuid().ToString(), "Harry", Book.SeriesEnum.HarryPotter);
-                SCart.Items.Add(tmpBook);
-            }
-            Assert.AreEqual<int>(100, SCart.Count());
+      ShoppingCart SCart = new ShoppingCart();
+      //Creation of 100 books
+      for (int i = 0; i < 100; i++)
+      {
+        Book tmpBook = new Book(
+            (100 * i), "TestBook", Guid.NewGuid().ToString(), "Harry", Book.SeriesEnum.HarryPotter);
+        SCart.Items.Add(tmpBook);
+      }
+      Assert.AreEqual<int>(100, SCart.Count());
 
-            //Add 20 copies
-            SCart.AddToShoppingCart(Utils.GetSecondBook(), 20);
-            Assert.AreEqual<int>(120, SCart.Count());
-        }
-        [TestMethod]
-        public void CalcTotPrice()
-        {
-            ShoppingCart SCart = new ShoppingCart();
-
-            ////1 copy, 1 book
-            //SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
-            //Assert.AreEqual<double>(8, Calculator.CalcTotalPrice(SCart));
-            //SCart.Items.Clear();
-
-            ////2 copies, 1book
-            //SCart.AddToShoppingCart(Utils.GetFirstBook(), 2);
-            //Assert.AreEqual<double>(16, Calculator.CalcTotalPrice(SCart));
-            //SCart.Items.Clear();
-
-            ////2 different books
-            //SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetSecondBook(), 1);
-            //Assert.AreEqual<double>(15.2, Calculator.CalcTotalPrice(SCart));
-            //SCart.Items.Clear();
-
-            ////3 different books
-            //SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetSecondBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetThirdBook(), 1);
-            //Assert.AreEqual<double>(21.6, Calculator.CalcTotalPrice(SCart));
-            //SCart.Items.Clear();
-
-            ////4 different books
-            //SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetSecondBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetThirdBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetFourthBook(), 1);
-            //Assert.AreEqual<double>(25.6, Calculator.CalcTotalPrice(SCart));
-            //SCart.Items.Clear();
-
-            //5 different books
-            //SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetSecondBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetThirdBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetFourthBook(), 1);
-            //SCart.AddToShoppingCart(Utils.GetFifthBook(), 1);
-            //Assert.AreEqual<double>(40, Calculator.CalcTotalPrice(SCart));
-            //SCart.Items.Clear();
-
-            //5 different books with 2 copies
-            SCart.AddToShoppingCart(Utils.GetFirstBook(), 2);
-            SCart.AddToShoppingCart(Utils.GetSecondBook(), 2);
-            SCart.AddToShoppingCart(Utils.GetThirdBook(), 2);
-            SCart.AddToShoppingCart(Utils.GetFourthBook(), 2);
-            SCart.AddToShoppingCart(Utils.GetFifthBook(), 2);            
-            Assert.AreEqual<double>(60, Calculator.CalcTotalPrice(SCart));           
-            SCart.Items.Clear();
-        }
+      //Add 20 copies
+      SCart.AddToShoppingCart(Utils.GetSecondBook(), 20);
+      Assert.AreEqual<int>(120, SCart.Count());
     }
+
+    [TestMethod]
+    public void CalcTotPrice()
+    {
+      ShoppingCart SCart = new ShoppingCart();
+      /*
+      ////1 copy, 1 book
+      SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
+      Assert.AreEqual<double>(8, Calculator.CalcTotalPrice(SCart));
+      SCart.Items.Clear();
+
+      ////2 copies, 1book
+      SCart.AddToShoppingCart(Utils.GetFirstBook(), 2);
+      Assert.AreEqual<double>(16, Calculator.CalcTotalPrice(SCart));
+      SCart.Items.Clear();
+
+      ////5 copies, 1book
+      SCart.AddToShoppingCart(Utils.GetFirstBook(), 5);
+      Assert.AreEqual<double>(40, Calculator.CalcTotalPrice(SCart));
+      SCart.Items.Clear();
+       * */
+
+
+      ////2 different books
+      SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
+      SCart.AddToShoppingCart(Utils.GetSecondBook(), 1);
+      Assert.AreEqual<double>(15.2, Calculator.CalcTotalPrice(SCart));
+      SCart.Items.Clear();
+
+      ////3 different books
+      //SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
+      //SCart.AddToShoppingCart(Utils.GetSecondBook(), 1);
+      //SCart.AddToShoppingCart(Utils.GetThirdBook(), 1);
+      //Assert.AreEqual<double>(21.6, Calculator.CalcTotalPrice(SCart));
+      //SCart.Items.Clear();
+
+      ////4 different books
+      //SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
+      //SCart.AddToShoppingCart(Utils.GetSecondBook(), 1);
+      //SCart.AddToShoppingCart(Utils.GetThirdBook(), 1);
+      //SCart.AddToShoppingCart(Utils.GetFourthBook(), 1);
+      //Assert.AreEqual<double>(25.6, Calculator.CalcTotalPrice(SCart));
+      //SCart.Items.Clear();
+
+      //5 different books
+      //SCart.AddToShoppingCart(Utils.GetFirstBook(), 1);
+      //SCart.AddToShoppingCart(Utils.GetSecondBook(), 1);
+      //SCart.AddToShoppingCart(Utils.GetThirdBook(), 1);
+      //SCart.AddToShoppingCart(Utils.GetFourthBook(), 1);
+      //SCart.AddToShoppingCart(Utils.GetFifthBook(), 1);
+      //Assert.AreEqual<double>(40, Calculator.CalcTotalPrice(SCart));
+      //SCart.Items.Clear();
+
+      //5 different books with 2 copies
+      //SCart.AddToShoppingCart(Utils.GetFirstBook(), 2);
+      //SCart.AddToShoppingCart(Utils.GetSecondBook(), 2);
+      //SCart.AddToShoppingCart(Utils.GetThirdBook(), 2);
+      //SCart.AddToShoppingCart(Utils.GetFourthBook(), 2);
+      //SCart.AddToShoppingCart(Utils.GetFifthBook(), 2);            
+      //Assert.AreEqual<double>(60, Calculator.CalcTotalPrice(SCart));           
+      //SCart.Items.Clear();
+    }
+  }
 }
